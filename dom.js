0c52d76aecf77e9bdce719ffd4664456210f54d0
Manish-0003 <manish.sharma9314@gmail.com>
2023-07-10 22:05:37 +0530

    10th july task 9

--- dom.js
+++ dom.js
@@ -10,47 +10,44 @@ itemList.addEventListener('click', removeItem);
 filter.addEventListener('keyup', filterItems);
 
 // Add item
-function addItem(e){
+function addItem(e) {
   e.preventDefault();
 
-  // Get input value
+  // Get input values
   var newItem = document.getElementById('item').value;
+  var newDescription = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
-  // Add class
   li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+
+  // Create a span for item name and description
+  var itemText = document.createElement('span');
+  itemText.textContent = newItem + '  ' + newDescription;
+
+  // Append text span to li
+  li.appendChild(itemText);
 
   // Create del button element
   var deleteBtn = document.createElement('button');
-
-  // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
 
-  // Create edit button element
-  var editBtn = document.createElement('button');
-  // Add classes to edit button
-  editBtn.className = 'btn btn-primary btn-sm float-right edit';
-  // Append text node to edit button
-  editBtn.appendChild(document.createTextNode('Edit'));
-
-  // Append buttons to li
+  // Append delete button to li
   li.appendChild(deleteBtn);
-  li.appendChild(editBtn);
 
   // Append li to list
   itemList.appendChild(li);
+
+  // Clear input fields
+  document.getElementById('item').value = '';
+  document.getElementById('description').value = '';
 }
 
 // Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure?')) {
       var li = e.target.parentElement;
       itemList.removeChild(li);
     }
@@ -58,15 +55,12 @@ function removeItem(e){
 }
 
 // Filter Items
-function filterItems(e){
-  // convert text to lowercase
+function filterItems(e) {
   var text = e.target.value.toLowerCase();
-  // Get lis
   var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
+  Array.from(items).forEach(function (item) {
+    var itemName = item.textContent;
+    if (itemName.toLowerCase().indexOf(text) !== -1) {
       item.style.display = 'block';
     } else {
       item.style.display = 'none';
